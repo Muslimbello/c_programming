1476828a13a489ea57b6ad67baee1416795d3a89
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 <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>
2024-02-28 23:23:18 +0100

    Path location

--- myshell/main.c
+++ myshell/main.c
@@ -1,47 +1,124 @@
+// #include "main.h"
+
+// /**
+//   * main - Getline function
+//   * @argc: Argument count
+//   * @argv: Array of argument values
+//   *
+//   * Return: 0 on success
+//   */
+
+// int main(int argc, char **argv)
+// {
+//         (void)argc, (void)argv;
+//         char *buf = NULL, *token, *path;
+//         size_t count = 0;
+//         ssize_t nread;
+//         pid_t child_pid;
+//         int i, status;
+//         char **array;
+
+//         while (1)
+//         {
+//                 write(STDOUT_FILENO, "MyShell$ ", 9);
+
+//                 nread = getline(&buf, &count, stdin);
+
+//                 if (nread ==  -1)
+//                 {
+//                         perror("Exiting shell");
+//                         exit(0);
+//                 }
+
+//                 token = strtok(buf, " \n");
+
+//                 array = malloc(sizeof(char*) * 1024);
+//                 i = 0;
+
+//                 while (token)
+//                 {
+//                         array[i] = token;
+//                         token = strtok(NULL, " \n");
+//                         i++;
+//                 }
+
+//                 array[i] = NULL;
+
+//                 path = get_file_path(array[0]); /* Get the absolute path using the function */
+
+//                 child_pid = fork();
+
+//                 if (child_pid == -1)
+//                 {
+//                         perror("Failed to create.");
+//                         exit (41);
+//                 }
+
+//                 if (child_pid == 0)
+//                 {
+//                         if (execve(path, array, NULL) == -1) /* Replace the first argument with that executable file */
+//                         {
+//                                 perror("Failed to execute");
+//                                 exit(97);
+//                         }
+//                 }
+//                 else
+//                 {
+//                         wait(&status);
+//                 }
+//         }
+//         free(path);
+//         free(buf);
+//         return (0);
+// }
 #include "main.h"
+
 /**
- * main - Main entry point for our program
- * @argc: Argument count to the main
- * @argv: Pointer to array of argument values
+ * main - Getline function
+ * @argc: Argument count
+ * @argv: Array of argument values
  *
- * Return: O Always success
+ * Return: 0 on success
  */
 
 int main(int argc, char **argv)
 {
 	(void)argc, (void)argv;
-	char *buff = NULL;
+	char *buf = NULL, *token, *path;
 	size_t count = 0;
-	ssize_t nread = 0;
+	ssize_t nread;
 	pid_t child_pid;
-	int status;
-	char *token;
+	int i, status;
 	char **array;
-	int i;
-	char name[] = "Myshell$";
-	int len_name = strlen(name);
 
 	while (1)
 	{
-		write(STDERR_FILENO, name, len_name);
-		nread = getline(&buff, &count, stdin);
-		token = strtok(buff, " \n");
+		write(STDOUT_FILENO, "MyShell$ ", 9);
 
-		array = malloc(sizeof(char *) * 1024);
+		nread = getline(&buf, &count, stdin);
 
 		if (nread == -1)
 		{
-			perror("Exit shell");
-			exit(1);
+			perror("Exiting shell");
+			exit(0);
 		}
+
+		token = strtok(buf, " \n");
+
+		array = malloc(sizeof(char *) * 1024);
 		i = 0;
+
 		while (token)
 		{
 			array[i] = token;
 			token = strtok(NULL, " \n");
 			i++;
 		}
+
 		array[i] = NULL;
+
+		path = get_file_path(array[0]); /* Get the absolute path using the function */
+
 		child_pid = fork();
 
 		if (child_pid == -1)
@@ -52,88 +129,18 @@ int main(int argc, char **argv)
 
 		if (child_pid == 0)
 		{
-			if (execve(array[0], array, NULL) == -1)
+			if (execve(path, array, NULL) == -1) /* Replace the first argument with that executable file */
 			{
-				perror("couldn't execute");
-				exit(1);
+				perror("Failed to execute");
+				exit(97);
 			}
 		}
 		else
 		{
-			/* Wait for the child process to execute before terminating the parent process */
 			wait(&status);
 		}
 	}
-	free(buff);
+	free(path);
+	free(buf);
 	return (0);
 }
-// #include "main.h"
-
-// /**
-//  * main - Getline function
-//  * @argc: Argument count
-//  * @argv: Array of argument values
-//  *
-//  * Return: 0 on success
-//  */
-
-// int main(int argc, char **argv)
-// {
-// 	(void)argc, (void)argv;
-// 	char *buf = NULL, *token;
-// 	size_t count = 0;
-// 	ssize_t nread;
-// 	pid_t child_pid;
-// 	int i, status;
-// 	char **array;
-
-// 	while (1)
-// 	{
-// 		write(STDOUT_FILENO, "MyShell$ ", 9);
-
-// 		nread = getline(&buf, &count, stdin);
-
-// 		if (nread == -1)
-// 		{
-// 			perror("Exiting shell");
-// 			exit(1);
-// 		}
-
-// 		token = strtok(buf, " \n");
-
-// 		array = malloc(sizeof(char *) * 1024);
-// 		i = 0;
-
-// 		while (token)
-// 		{
-// 			array[i] = token;
-// 			token = strtok(NULL, " \n");
-// 			i++;
-// 		}
-
-// 		array[i] = NULL;
-
-// 		child_pid = fork();
-
-// 		if (child_pid == -1)
-// 		{
-// 			perror("Failed to create.");
-// 			exit(41);
-// 		}
-
-// 		if (child_pid == 0)
-// 		{
-// 			if (execve(array[0], array, NULL) == -1)
-// 			{
-// 				perror("Failed to execute");
-// 				exit(97);
-// 			}
-// 		}
-// 		else
-// 		{
-// 			wait(&status);
-// 		}
-// 	}
-// 	free(buf);
-// 	return (0);
-// }
--- myshell/main.h
+++ myshell/main.h
@@ -5,6 +5,11 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+char *get_file_path(char *file_name);
+char *get_file_loc(char *path, char *file_name);
+int startsWithForwardSlash(const char *str);
+
 #endif
